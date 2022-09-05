e884918083c7babdd78f20b0192f36847778bde0
Madhavaraman V <101283753+MadhavaramanVR@users.noreply.github.com>
2022-09-05 15:28:11 +0530

    Sample main created
    Base file created

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter two numbers");
+    int num1=sc.nextInt();
+    int num2=sc.nextInt();
+    Main ob=new Main();
+  }
+}
