21b65f7c08a65fd7587aeecbb11d6e2a5de97217
Madhavaraman V <101283753+MadhavaramanVR@users.noreply.github.com>
2022-09-06 11:06:08 +0530

    Update Squash commit.java

--- Squash commit.java
+++ Squash commit.java
@@ -7,4 +7,13 @@ class Main
   System.out.println("Enter a string ");
   String str=sc.nextLine();
   }
+  void print(String s)
+  {
+    for(int i=0;i<s.length;i++)
+    {
+      System.out.println(s.charAt(i));
+    }
+    
+  }
+  
 }
